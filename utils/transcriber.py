20228bda282b079ea30ab60879965f6f8edf0bce
CODE SOFT <korrapraveen2005@gmail.com>
2025-02-21 16:30:05 +0530

    Add files via upload

---
+++ utils/transcriber.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+
+def transcribe_audio(audio_path):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(audio_path) as source:
+        audio_data = recognizer.record(source)
+        try:
+            return recognizer.recognize_google(audio_data)
+        except sr.UnknownValueError:
+            return "Could not understand audio"
+        except sr.RequestError:
+            return "Error with the speech recognition service"
